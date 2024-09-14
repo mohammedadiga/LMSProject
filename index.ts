bcb7d70b3c44201bae3e939ac62c5e86a87c3342
Adigamahoo <mohammed.adiga1122@gmail.com>
2024-09-14 10:37:47 +0300

    Started LMS Project

---
+++ index.ts
@@ -0,0 +1,41 @@
+import express, { NextFunction, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+
+export const app = express();
+
+// body parser
+app.use(express.json({limit: "50mb"}));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors => cross origin resource sharing
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+// testing api
+app.get('/test', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({ 
+        success: true,
+        message: 'API is working fine!'
+    });
+});
+
+// all api
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    // const err = new Error(`Route ${req.originalUrl} not found`) as any;`
+    // err.statusCode = 404;
+    // next(err);
+    res.status(404).json({ 
+        success: false,
+        message: `Route ${req.originalUrl} not found`
+    });
+});
+
+// create server
+app.listen(process.env.PORT, () => {
+    console.log(`Server is connected with port ${process.env.PORT}`);
+});
